7c62df3b946e3b76b4f4693c534aa315f49ef885
Daniele Pizzoni <danielepizzoni@gmail.com>
2012-11-09 11:48:00 +0100

    Fix crash when zooming out

--- ADClusterMapView/ADClusterAnnotation.h
+++ ADClusterMapView/ADClusterAnnotation.h
@@ -10,7 +10,9 @@
 #import <MapKit/MapKit.h>
 #import "ADMapCluster.h"
 
-#define kADCoordinate2DOffscreen CLLocationCoordinate2DMake(85.0, 179.0) // this coordinate puts the annotation on the top right corner of the map. We use this instead of kCLLocationCoordinate2DInvalid so that we don't mess with MapKit's KVO weird behaviour that removes from the map the annotations whose coordinate was set to kCLLocationCoordinate2DInvalid.
+//#define kADCoordinate2DOffscreen CLLocationCoordinate2DMake(85.0, 179.0) // this coordinate puts the annotation on the top right corner of the map. We use this instead of kCLLocationCoordinate2DInvalid so that we don't mess with MapKit's KVO weird behaviour that removes from the map the annotations whose coordinate was set to kCLLocationCoordinate2DInvalid.
+
+#define kADCoordinate2DOffscreen CLLocationCoordinate2DMake(361.0, 361.0)
 
 BOOL ADClusterCoordinate2DIsOffscreen(CLLocationCoordinate2D coord);
 
